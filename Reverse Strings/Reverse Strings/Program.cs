1df18e5c8905eb2c4da1912d3c9edb1d52721bbb
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-25 19:17:49 +0300

    add

---
+++ Reverse Strings/Reverse Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+            ReverseStrings(stack,input);
+            PoolString(stack);
+
+           
+        }
+
+        private static void PoolString(Stack<char> stack)
+        {
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+
+        private static Stack<char> ReverseStrings(Stack<char> stack,string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            return stack;
+        }
+    }
+}
