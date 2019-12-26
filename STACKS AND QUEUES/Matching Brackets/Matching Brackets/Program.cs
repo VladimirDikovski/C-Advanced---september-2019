ca2301b0ea062430d1b9a334d557fa3e7560b15c
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-26 09:59:21 +0300

    add

---
+++ STACKS AND QUEUES/Matching Brackets/Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+            FindBrackets(input, stack);
+        }
+
+        private static void FindBrackets(string input, Stack<int> stack)
+        {
+            for (int i = 0; i <input.Length; i++)
+            {
+                var symbol = input[i];
+                if (symbol =='(')
+                {
+                    stack.Push(i);
+                }
+                else if (symbol ==')')
+                {
+                    var closeBraketindex = stack.Pop();
+                    var result = input.Substring(closeBraketindex, i-closeBraketindex+1);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
