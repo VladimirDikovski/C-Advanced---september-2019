b636d35921b3a1b1c647abfa186adf716919757f
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-30 10:54:22 +0300

    add

---
+++ STACKS AND QUEUES/Crossroads/Crossroads/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int durationGreenLight = int.Parse(Console.ReadLine());
+            int holdValueOfGreenLight = durationGreenLight;
+            int FreeWindows = int.Parse(Console.ReadLine());
+            var cars = new Queue<string>();
+            int passedCars = 0;
+            bool isCrashHappen = false;
+            char ch = ' ';
+            string crashCars = string.Empty;
+
+            while (true)
+            {
+                if (isCrashHappen)
+                {
+                    break;
+                }
+                string carsOrCommand = Console.ReadLine();
+                if (carsOrCommand == "END")
+                {
+                    break;
+                }
+                if (carsOrCommand != "green")
+                {
+                      
+                  cars.Enqueue(carsOrCommand);                   
+                  
+                }
+                else if (carsOrCommand == "green")
+                {
+                   durationGreenLight = holdValueOfGreenLight;
+                      while (cars.Count>0)
+                    {
+                        var currentCar = cars.Peek();
+
+                        if (currentCar.Length <= durationGreenLight + FreeWindows)
+                        {
+                            passedCars++;
+                            durationGreenLight -= currentCar.Length;
+                            cars.Dequeue();
+                        }
+                        else
+                        {
+                            if (durationGreenLight < 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                var sum = durationGreenLight + FreeWindows;
+                                crashCars = currentCar;
+                                isCrashHappen = true;
+                                 ch = currentCar[sum];
+
+                                break;
+                            }
+                        }
+                      
+                    }
+                
+                }
+            }
+            if (isCrashHappen)
+            {
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{crashCars} was hit at {ch}.");
+            }
+            else
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{passedCars} total cars passed the crossroads.");
+            }
+
+        }
+    }
+}
---
+++ STACKS AND QUEUES/Simple Text Editor/Simple Text Editor/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOperation = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+            var stack = new Stack<string>();
+            
+            for (int i = 0; i < countOperation; i++)
+            {
+                var currentElemnt = Console.ReadLine().Split();
+                var command = currentElemnt[0];
+                switch (command)
+                {
+                    case "1":
+                         text = AddText(currentElemnt,text);
+                        stack.Push(text);
+                        break;
+                    case "2":
+                        text = RemoveElemnts(currentElemnt, text);
+                        stack.Push(text);
+                        break;
+                    case "3":
+                        ReturnElement(currentElemnt, text);
+                         break;
+                    case "4":
+                        
+                       text = UndoOperation(stack, text);
+                        break;
+                }            
+            }
+        }
+
+        private static string UndoOperation(Stack<string> stack, string text)
+        {
+            stack.Pop();
+            if (stack.Count > 0)
+            {
+                text = stack.Peek();
+            }
+            else
+            {
+                text = string.Empty;
+            }
+            return text;
+
+        }
+
+        private static string RemoveElemnts(string[] currentElemnt, string text)
+        {
+            int countElemnts = int.Parse(currentElemnt[1]);
+           
+                for (int i = countElemnts; i > 0; i--)
+                {
+                    text = text.Remove(text.Length - 1);
+                }
+           
+            return text;
+        }
+
+        private static void ReturnElement(string[] currentElemnt, string text)
+        {
+            var index = int.Parse(currentElemnt[1]);
+           
+                Console.WriteLine(text[index - 1]);
+          
+        }  
+
+        private static string AddText(string[] currentElemnt, string text)
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(currentElemnt[1]);
+            text += str;
+            return text;
+
+        }
+    }
+}
