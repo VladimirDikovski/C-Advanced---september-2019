f971243af79b1a47967f568b7edb68a025ca504a
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-31 18:39:44 +0300

    add

---
+++ Multidimensional Arrays/Sum Matrix Columns/Sum Matrix Columns/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ColsAndRows = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rols = ColsAndRows[0];
+            int cols = ColsAndRows[1];
+            int[,] matrix = new int[rols, cols];
+            var sumOfColons = new List<int>();
+            InputElementsInMatrix(rols, cols, matrix);
+            FindSumOfColons(matrix, rols, cols,sumOfColons);
+            PrintElements(sumOfColons);
+        }
+
+        private static void PrintElements(List<int> sumOfColons)
+        {
+            foreach (var items in sumOfColons)
+            {
+                Console.WriteLine(items);
+            }
+        }
+
+        private static List<int> FindSumOfColons(int[,] matrix, int rols, int cols,List<int>sumOfColons)
+        {
+            
+            int sum = 0;
+            for (int col = 0; col < cols; col++)
+            {
+                for (int rol = 0; rol < rols; rol++)
+                {
+                    sum += matrix[rol, col];
+                }
+                sumOfColons.Add(sum);
+                sum = 0;
+            }
+            return sumOfColons;
+        }
+
+        private static int[,] InputElementsInMatrix(int rols, int cols, int[,] matrix)
+        {
+            for (int rol = 0; rol <rols; rol++)
+            {
+                int[] elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[rol, col] = elements[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Multidimensional Arrays/Sum Matrix Elements/Sum Matrix Elements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] colsAndRows = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = colsAndRows[0];
+            int cols = colsAndRows[1];
+            int[,] matrix = new int[rows, cols];
+            AddElementsInMatrix(cols, rows, matrix, colsAndRows);
+            PrintCountRowAndCol(rows, cols);
+            PrintSumOfAllElements(matrix);
+
+        }
+
+        private static void PrintSumOfAllElements(int[,] matrix)
+        {
+            int sumOFelements = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sumOFelements += matrix[row, col];
+
+                }
+            }
+            Console.WriteLine(sumOFelements);
+        }
+
+        private static void PrintCountRowAndCol(int rows, int cols)
+        {
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+        }
+
+        private static int[,] AddElementsInMatrix(int cols, int rows, int[,] matrix, int[] colsAndRows)
+        {
+
+            for (int row = 0; row <rows; row++)
+            {
+                int[] elements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
