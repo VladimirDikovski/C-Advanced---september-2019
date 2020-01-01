2f22e14a458214cd28914d0be90230db166e7302
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-01-01 13:01:59 +0300

    add

---
+++ Multidimensional Arrays/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[,] matrixi1 = new int[3, 3];
+            int[,] matrixi2 = new int[3, 3];
+            Console.WriteLine("Input Element of the first Matrix ");
+            matrixi1 = InputElementsInMatrix(matrixi1);
+            Console.WriteLine();
+            Console.WriteLine("Input Element of the second Matrix ");
+            matrixi2 = InputElementsInMatrix(matrixi2);
+            Console.WriteLine();
+            Console.WriteLine("firstMatrix" );
+            PrintMaxtrix(matrixi1);
+            Console.WriteLine();
+            Console.WriteLine("secondMatrix");
+            PrintMaxtrix(matrixi2);
+            MatrixMultiplication(matrixi1, matrixi2);
+        }
+
+        private static void MatrixMultiplication(int[,] matrixi1, int[,] matrixi2)
+        {
+            int result = 0;
+            for (int rol = 0; rol < matrixi1.GetLength(0); rol++)
+            {
+                for (int col = 0; col < matrixi2.GetLength(1); col++)
+                {
+                    result += matrixi1[col, rol] * matrixi2[rol, col];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Result of multiplication of matrix is {result}");
+        }
+
+        private static void PrintMaxtrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]+" ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        private static int[,] InputElementsInMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            return matrix;
+        }
+          
+    }
+    
+}
