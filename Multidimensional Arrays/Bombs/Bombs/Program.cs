4d37cf2342dd01f62dbde9f372008ba398d548d3
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-01-04 13:08:11 +0300

    add

---
+++ Multidimensional Arrays/Bombs/Bombs/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+namespace Bombs
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int currentbombRow;
+        static int currentBombCol;
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            matrix = new int[size, size];
+            InputElements();
+            BombsExplosion();
+            PrintAliveCells();
+        }
+        private static void PrintAliveCells()
+        {
+            int sum = 0;
+            int countAliveCells = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {             
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        sum += matrix[row, col];
+                        countAliveCells++;
+                    }              
+                }
+            }          
+                Console.WriteLine($"Alive cells: {countAliveCells}");
+                Console.WriteLine($"Sum: {sum}");
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+
+                        Console.Write(matrix[row, col] + " ");
+                    }
+                    Console.WriteLine();
+                }           
+        }
+        private static void BombsExplosion()
+        {
+            int[] bombs = Console.ReadLine().Split(new char[] { ',', ' ' }).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < bombs.Length / 2; i++)
+            {
+                currentbombRow = bombs[i + i];
+                currentBombCol = bombs[i + i + 1];
+                if (matrix[currentbombRow, currentBombCol] > 0)
+                {
+                if (isvalidCordinates(-1, 0))
+                {
+                    matrix[currentbombRow - 1, currentBombCol] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(-1, +1))
+                {
+                    matrix[currentbombRow - 1, currentBombCol + 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(-1, -1))
+                {
+                    matrix[currentbombRow - 1, currentBombCol - 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(0, -1))
+                {
+                    matrix[currentbombRow, currentBombCol - 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(0, +1))
+                {
+                    matrix[currentbombRow, currentBombCol + 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(+1, 0))
+                {
+                    matrix[currentbombRow + 1, currentBombCol] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(+1, -1))
+                {
+                    matrix[currentbombRow + 1, currentBombCol - 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                if (isvalidCordinates(+1, +1))
+                {
+                    matrix[currentbombRow + 1, currentBombCol + 1] -= matrix[currentbombRow, currentBombCol];
+                }
+                 matrix[currentbombRow, currentBombCol] = 0;
+            }
+               
+            }          
+        }
+        private static bool isvalidCordinates(int row,int col)
+        {
+            return row + currentbombRow >= 0 && row + currentbombRow < matrix.GetLength(0) && col + currentBombCol >= 0 && col + currentBombCol < matrix.GetLength(1)
+                && matrix[row + currentbombRow, col + currentBombCol] > 0;
+        }
+
+        private static void InputElements()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays/Miner/Miner/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace Miner
+{
+   
+    class Program
+    {
+        static char[,] matrix;
+        static int coals;
+        static int starttRow;
+        static int starttCol;
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] directions = Console.ReadLine().Split(" ");
+            matrix = new char [size, size];
+            InpitElements();
+           
+            foreach (var item in directions)
+            {
+                switch (item)
+                {
+                    case "left":
+                        Moves(0, -1);
+                        break;
+                    case "right":
+                        Moves(0, +1);
+                        break;
+                    case "up":
+                        Moves(-1, 0);
+                        break;
+                    case "down":
+                        Moves(+1, 0);
+                        break;
+
+                }
+            }
+            Console.WriteLine($"{coals} coals left. ({starttRow}, {starttCol})");
+
+        }
+
+        private static void Moves(int row, int col)
+        {
+           
+            if(isValid(starttRow+row,starttCol+col))
+            {
+                starttRow += row;
+                starttCol += col;
+
+                if(matrix[starttRow,starttCol]=='e')
+                {
+                    Console.WriteLine($"Game over! ({starttRow}, {starttCol})");
+                    System.Environment.Exit(0);
+                }
+                else if(matrix[starttRow, starttCol] == 'c')
+                {
+                    coals--;
+                    matrix[starttRow,starttCol] = '*';
+                    if (coals == 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({starttRow}, {starttCol})");
+                        System.Environment.Exit(0);
+                    }
+                }
+            }
+        }
+
+        private static bool isValid(int starttRow, int starttCol)
+        {
+            return starttRow >= 0 && starttRow < matrix.GetLength(0) && starttCol >= 0
+                && starttCol < matrix.GetLength(1);
+        }
+
+        private static void InpitElements()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] elements = Console.ReadLine().Split(" ").Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                    if (matrix[row,col] == 'c')
+                    {
+                        coals++;
+                    }
+                    if (matrix[row, col] == 's')
+                    {
+                        starttRow = row;
+                        starttCol = col;
+                    }
+                }
+            }
+        }
+    }
+}
