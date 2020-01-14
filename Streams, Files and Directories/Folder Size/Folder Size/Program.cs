3f6b2bb1672f0341632808c21e5fae465bbb185e
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-01-14 19:56:12 +0300

    add

---
+++ Streams, Files and Directories/Folder Size/Folder Size/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("TestFolder");
+            double sum = 0;
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("output.txt", sum.ToString());
+           
+        }
+    }
+}
