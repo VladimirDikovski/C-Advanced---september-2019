bdd4b26456010e0d870da12a2490500b25125dec
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-01-14 19:34:27 +0300

    add

---
+++ Streams, Files and Directories/Slice a File/Slice a File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filesInfo = new FileInfo("sliceMe.txt");
+            var SizePerfile = (int)Math.Ceiling(filesInfo.Length / 5.0);
+            using (var r =new FileStream("SliceMe.txt", FileMode.Open))
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    var buffer = new byte[SizePerfile];
+                    var readBytes = r.Read(buffer, 0, SizePerfile);
+                    using(var w =new FileStream($"file-{i}.txt", FileMode.OpenOrCreate))
+                    {
+                        w.Write(buffer, 0, readBytes);
+                    }
+
+                }
+                
+                
+            }
+
+        }
+    }
+}
