6d4bd89a51eb7c893e172a34804dd1acea66b395
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-01-23 19:53:36 +0300

    add

---
+++ Functional Programming/List of Predicates/List of Predicates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            bool isDivede = false;
+            var result = new List<int>();
+            FindDividers(n, dividers, isDivede, result);
+
+
+
+        }
+
+        private static void FindDividers(int n, int[] dividers, bool isDivede, List<int> result)
+        {
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int j = 0; j < dividers.Length; j++)
+                {
+                    if (i % dividers[j] == 0)
+                    {
+                        isDivede = true;
+                    }
+                    else
+                    {
+                        isDivede = false;
+                        break;
+
+                    }
+                }
+                if (isDivede)
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
---
+++ Functional Programming/TriFunction/TriFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charLenght = int.Parse(Console.ReadLine());
+            var listOfNames = Console.ReadLine().Split(" ").ToList();
+           
+            Func<string, int, bool> IsGrater = (x, y) => x.Sum(c=>c) >= y;
+            Func<Func<string, int, bool>, List<string>, string> returnFirst = (a, b) => b.FirstOrDefault(s => a(s, charLenght));
+            string result = returnFirst(IsGrater, listOfNames);
+            if (result != null)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(String.Empty);
+            }
+        }
+
+      
+    }
+}
