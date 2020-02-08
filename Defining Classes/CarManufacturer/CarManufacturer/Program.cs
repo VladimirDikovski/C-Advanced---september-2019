ac5462723327d9b9563a81af3a741350209d1259
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-02-08 19:17:05 +0300

    add

---
+++ Defining Classes/CarManufacturer/CarManufacturer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "VW";
+            car.Model = "GOLF";
+            car.Year = 2000;
+
+        }
+    }
+}
