35046a1fac525769fbbad8153664ec238ac3c3fa
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-02-09 14:43:15 +0300

    add

---
+++ Defining Classes/Special Cars/Special Cars/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car(string make, string model, int year, double fuelQuontity, double fuelconsumption, Engine engine, Tire[] tires)
+            :this(make,model,year,fuelQuontity,fuelconsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;            
+        }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+        public Car(string make,string model,int year)
+            :this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            
+        }
+        public Car(string make,string model,int year, double fuelQuontity ,double fuelconsumption)
+            :this(make,model,year)
+        {
+            this.FuelQuantity = fuelQuontity;
+            this.FuelConsumption = fuelconsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            double expenceFuel = this.FuelConsumption * distance / 100;
+            if (expenceFuel > this.FuelQuantity)
+            {
+                throw new ArgumentException("Not enough fuel to perform this trip!");
+
+            }
+            else
+            {
+                this.FuelQuantity -= distance / 100 * this.FuelConsumption;
+            }
+        }
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+            return result.ToString();
+
+        }
+    }
+}
---
+++ Defining Classes/Special Cars/Special Cars/StartUp.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var tires = new List<Tire[]>();
+            var engines = new List<Engine>();
+            var cars = new List<Car>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input== "No more tires")
+                {
+                    break;
+                }
+                var splitInput = input.Split(" ");
+                var curretTires = new Tire[4]
+                {
+                    new Tire(int.Parse(splitInput[0]),double.Parse(splitInput[1])),
+                    new Tire(int.Parse(splitInput[2]),double.Parse(splitInput[3])),
+                    new Tire(int.Parse(splitInput[4]),double.Parse(splitInput[5])),
+                     new Tire(int.Parse(splitInput[6]),double.Parse(splitInput[7])),
+                };
+                tires.Add(curretTires);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Engines done")
+                {
+                    break;
+                }
+                var splitInput = input.Split(" ");
+                for (int i = 0; i < splitInput.Length / 2; i++)
+                {
+                    var horsepower = int.Parse(splitInput[i + i]);
+                    var cubicCapacity = double.Parse(splitInput[i + i + 1]);
+                    var engine = new Engine(horsepower, cubicCapacity);
+                    engines.Add(engine);
+
+                }
+               
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input== "Show special")
+                {
+                    break;
+                }
+                var splitInput = input.Split(" ");
+                var make = splitInput[0];
+                var model = splitInput[1];
+                var year = int.Parse(splitInput[2]);
+                var fuelquontity = double.Parse(splitInput[3]);
+                var fuelfuelConsumption = double.Parse(splitInput[4]);
+                var engineIndex = int.Parse(splitInput[5]);
+                var tiresIndex = int.Parse(splitInput[6]);
+                var car = new Car(make, model, year, fuelquontity, fuelfuelConsumption, engines[engineIndex], tires[tiresIndex]);
+                cars.Add(car);
+
+            }
+
+            foreach (var currenCar in cars)
+            {
+                double totV = 0;
+                bool pres = false;
+                bool isyear = false;
+                if (currenCar.Year >= 2017)
+                {
+                    isyear = true;
+                }
+                bool hp = false;
+                if (currenCar.Engine.HorsePower >= 330)
+                {
+                    hp = true;
+                }
+                foreach (var currTires in currenCar.Tires)
+                {
+                    double value = currTires.Pressure;
+                    totV = totV + value;
+                }
+                if (totV >= 9 && totV <= 10)
+                {
+                    pres = true;
+                }
+                if (isyear && hp && pres)
+                {
+                    currenCar.Drive(20);
+                    Console.WriteLine($"Make: {currenCar.Make}\nModel: {currenCar.Model}\nYear: {currenCar.Year}\nHorsePowers: {currenCar.Engine.HorsePower}\nFuelQuantity: {currenCar.FuelQuantity}");
+                }
+            }
+
+        }
+            
+        }
+    }
+
