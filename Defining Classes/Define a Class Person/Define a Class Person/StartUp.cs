857e746186054c9921b05a6501ad1c52ab377458
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-02-09 15:10:35 +0300

    add

---
+++ Defining Classes/Define a Class Person/Define a Class Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Pesho";
+            person.Year = 2000;
+
+            Console.WriteLine($"{person.Name} {person.Year}");
+        }
+    }
+}
