aa14083ad14db25e70c350f5596d2917b48198d4
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-03-28 19:49:05 +0300

    add

---
+++ Definiting Classes Exercise/Pokemon Trainer/Pokemon Trainer/StartUp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+      public  static void Main(string[] args)
+        {
+            var newPokemon = new Pokemon();
+            var ListOftrainer = new Dictionary<string, Trainer>();
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input== "Tournament")
+                {
+                    break;
+                }
+
+                string[] splitedInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string treinerName = splitedInput[0];
+                string pokemonName = splitedInput[1];
+                string pokomenElement = splitedInput[2];
+                int pokomonHealth = int.Parse(splitedInput[3]);
+                newPokemon = new Pokemon(pokemonName, pokomenElement, pokomonHealth);
+                var newTrainer = new Trainer();
+                newTrainer.Name = treinerName;
+                newTrainer.Pokemons.Add(newPokemon);
+                if (!ListOftrainer.ContainsKey(newTrainer.Name))
+                {
+                    ListOftrainer[newTrainer.Name] = newTrainer;
+                }
+                else
+                {
+                    ListOftrainer[newTrainer.Name].Pokemons.Add(newPokemon);
+                }
+          
+            }
+
+            while (true)
+            {
+                string elemental = Console.ReadLine();
+                if (elemental == "End")
+                {
+                    break;
+                }
+
+                foreach (var item in ListOftrainer.Values)
+                {
+                    if (item.Pokemons.Any(x=>x.Element==elemental))
+                    {                     
+                            item.NumberOfBadget += 1;                    
+                    }
+                    else
+                    {
+                        foreach (var helath in item.Pokemons )
+                        {
+                            helath.Health -= 10;
+                        }   
+                    }
+                    if(item.Pokemons.Any(x=>x.Health<=0))
+                    {
+                        for (int i = 0; i <item.Pokemons.Count; i++)
+                        {
+                            item.Pokemons.Remove(item.Pokemons[i]);
+                        }
+                    }
+                }
+              
+
+
+            }
+              foreach (var item in ListOftrainer)
+                {
+                    Console.WriteLine($"{item.Key} {item.Value.NumberOfBadget} {item.Value.Pokemons.Count}");
+                }
+
+        }
+    }
+}
---
+++ Definiting Classes Exercise/Pokemon Trainer/Pokemon Trainer/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace DefiningClasses
+{
+   public class Trainer
+    {
+        public string Name { get; set; }
+        public int NumberOfBadget { get; set; }
+        public List<Pokemon> Pokemons { get; set; } = new List<Pokemon>();
+
+        public Trainer()
+        {
+
+        }
+      
+    }
+}
