7c57a13f68f57f06c633c8b418e3f3e1e09917a4
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2020-03-29 09:36:17 +0300

    add

---
+++ Definiting Classes Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+  public  class Car
+    {
+        private string make;
+        private string model;
+        private int horsepower;
+        private string registretionNumber;
+
+        public Car()
+        {
+
+        }
+        public Car(string make,string model,int horsepower,string regnumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsepower;
+            this.RegistrationNumber = regnumber;
+        }
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsepower;
+            }
+            set
+            {
+                this.horsepower = value;
+            }
+        }
+        public string RegistrationNumber
+        {
+            get
+            {
+                return this.registretionNumber;
+            }
+            set
+            {
+                this.registretionNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make}\nModel: {Model}\nHorsePower: {HorsePower}\nRegistrationNumber: {RegistrationNumber}";
+          
+        }
+    }
+}
---
+++ Definiting Classes Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace SoftUniParking
+{
+   public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.cars = new List<Car>();
+            this.capacity = capacity;
+        }
+        public int Count
+        {
+            get
+            {
+                return this.cars.Count;
+            }
+        }
+
+        public string AddCar(Car car)
+        {
+            if (cars.Any(x => x.RegistrationNumber.Contains(car.RegistrationNumber)))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (cars.Count >= capacity)
+            {
+                return "Parking is full!";
+            }
+        
+            else
+            {
+                this.cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+          
+        }
+        public string RemoveCar(string RegistrationNumber)
+        {
+            if (!cars.Any(x => x.RegistrationNumber.Contains(RegistrationNumber)))
+            {
+               return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                cars.RemoveAll(x => x.RegistrationNumber == RegistrationNumber);
+                return $"Successfully removed {RegistrationNumber}";
+            
+            }
+        }
+        public Car GetCar(string RegistrationNumber)
+        {
+            return cars.Where(x => x.RegistrationNumber == RegistrationNumber).FirstOrDefault();
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            foreach (var item in RegistrationNumbers)
+            {
+                cars.RemoveAll(x => x.RegistrationNumber == item);
+            }
+        }
+
+    }
+}
